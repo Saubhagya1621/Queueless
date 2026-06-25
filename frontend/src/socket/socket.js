970bcb0592fd604c4f0ad74272c1f4f950efcd24
Saubhagya1621 <saubhagya1603@gmail.com>
2026-06-25 19:21:12 +0530

    fix:Configure production API and Socket.IO URLs

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:8000', {
+const socket = io('https://queueless-backend-05vg.onrender.com', {
   withCredentials: true,
   autoConnect: false
 })
