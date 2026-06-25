95647b640635f32ac54718a17c495827f5d063e9
Saubhagya1621 <saubhagya1603@gmail.com>
2026-06-26 00:39:00 +0530

    fix: stage socket config and force vercel rebuild

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 const socket = io('https://queueless-backend-05vg.onrender.com', {
   withCredentials: true,
-  autoConnect: false
+  autoConnect: true,
 })
 
 export default socket
